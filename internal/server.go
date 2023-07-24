e88e8e4c4d2d462e859863529eaa466bb57b9ed4
Rodina Mohamed <75133923+Rodina222@users.noreply.github.com>
2023-07-24 10:44:50 +0300

    change methods

--- internal/server.go
+++ internal/server.go
@@ -1,6 +1,7 @@
-package internal
+package server
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"net/http"
@@ -8,17 +9,18 @@ import (
 	"strings"
 )
 
-var (
-	ErrInvalidPort = errors.New("invalid port number insert a port from 1 to 65535")
-	ErrKeyNotFound = errors.New("entered key is not found")
-)
+// ErrInvalidPort is returned when the listening port is outside the range (from 1 to 65535)
+var ErrInvalidPort = errors.New("invalid port number insert a port from 1 to 65535")
 
+// App is a struct that involves the listening port number of the server
 type App struct {
 	listenPort int
 }
 
-func CreateApp(port int) (*App, error) {
+// NewApp  validates the user input port, and if it is valid, it assigns the app's port number to it.
+func NewApp(port int) (*App, error) {
 
+	// check input port from user
 	if port < 1 || port > 65535 {
 		return nil, ErrInvalidPort
 	}
@@ -26,30 +28,110 @@ func CreateApp(port int) (*App, error) {
 	return &App{listenPort: port}, nil
 }
 
-func (app *App) handleEnv(w http.ResponseWriter, r *http.Request) {
+// Run function calls registerHandler() method which is an internal method that calls other internal methods for running the app
+func (app *App) Run() error {
 
-	if strings.HasPrefix(r.URL.Path, "/env/") {
-		key := strings.TrimPrefix(r.URL.Path, "/env/")
+	return app.registerHandlers()
 
-		app.handleGetKey(w, r, key)
-	} else {
-		app.handleGetEnv(w, r)
+}
+
+func (app *App) registerHandlers() error {
+
+	router := http.NewServeMux()
+
+	router.HandleFunc("/env", app.envhandler)
+	router.HandleFunc("/env/", app.envhandler)
+
+	port := fmt.Sprintf(":%d", app.listenPort)
+	fmt.Println("Server started listening on port", port)
+	err := http.ListenAndServe(port, router)
+
+	if err == http.ErrServerClosed {
+		return fmt.Errorf("server closed suddenly")
 	}
+
+	if err != nil {
+		return fmt.Errorf("failed to start the server %w", err)
+	}
+
+	return nil
+
+}
+
+/*if r.Method != http.MethodGet {
+
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+}*/
+
+func (app *App) envhandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodGet {
+
+		err := errors.New("http method should be get")
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	key := strings.TrimPrefix(r.URL.Path, "/env")
+
+	if key == "" {
+
+		app.handleGetEnv(w, r)
+
+	} else {
+
+		app.handleGetKey(w, r)
+
+	}
+
 }
 
 func (app *App) handleGetEnv(w http.ResponseWriter, r *http.Request) {
 
+	envMap := make(map[string]string)
+
 	for _, environ := range os.Environ() {
-		fmt.Fprintln(w, environ)
+
+		pair := strings.SplitN(environ, "=", 2)
+
+		envMap[pair[0]] = pair[1]
 	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	// encoding the map into json format
+	err := json.NewEncoder(w).Encode(envMap)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
 }
 
-func (app *App) handleGetKey(w http.ResponseWriter, r *http.Request, key string) error {
-	value, ok := os.LookupEnv(key)
-	if !ok {
-		http.NotFound(w, r)
-		return ErrKeyNotFound
+func (app *App) handleGetKey(w http.ResponseWriter, r *http.Request) {
+
+	key := strings.TrimPrefix(r.URL.Path, "/env/")
+
+	value := os.Getenv(key)
+
+	if value == "" {
+
+		w.WriteHeader(http.StatusNotFound)
+		return
+
 	}
-	fmt.Fprintf(w, "%s=%s\n", key, value)
-	return nil
+
+	// encoding the value into json format
+	err := json.NewEncoder(w).Encode(value)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
 }
